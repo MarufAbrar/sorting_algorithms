793d3c6d5884d9586a93a5502378f41a8eafb0e8
MarufAbrar <maracode1@gmail.com>
2022-08-16 21:50:14 +0300

    now

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,68 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
+
 /**
- * max_val - gets max value from array
- * @array: pointer to array
- * @size: size of the array
- * Return: max value from array
+ * main - Entry point
+ *
+ * Return: Always 0
  */
-int max_val(int *array, size_t size)
+int main(void)
 {
-	int max = array[0];
-	size_t i;
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
 
-	for (i = 0; i < size; i++)
-	{
-		if (array[i] > max)
-			max = array[i];
-	}
-	return (max);
-}
-/**
- * radix_sort - sorts an array of integers is ASC
- * order implementing Radix Sort algorithm
- * @array: pointer to array
- * @size: size of the array
- */
-void radix_sort(int *array, size_t size)
-{
-	int *new_arr;
-	int i, max, e = 1;
-	int tam = size;
-
-	if (!array || size < 2)
-		return;
-
-	max = max_val(array, size);
-
-	new_arr = malloc(sizeof(int) * size);
-	while (max / e > 0)
-	{
-		int brews[20] = {0};
-
-		i = 0;
-
-		while (i < tam)
-		{
-			brews[(array[i] / e) % 10]++;
-			i++;
-		}
-
-		if (brews != NULL)
-		{
-			for (i = 1; i < 10; i++)
-				brews[i] += brews[i - 1];
-
-			for (i = tam - 1; i >= 0; i--)
-			{
-				new_arr[brews[(array[i] / e) % 10] - 1] = array[i];
-				brews[(array[i] / e) % 10]--;
-			}
-
-			for (i = 0; i < tam; i++)
-				array[i] = new_arr[i];
-		}
-		e *= 10;
-		print_array(array, size);
-	}
-	free(new_arr);
+    print_array(array, n);
+    printf("\n");
+    radix_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
 }
